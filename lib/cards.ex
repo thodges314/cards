52b5f11abda72e5506c3c9367cc2354f0b3038ca
Thomas Hodges <thodges@javascriptonline.org>
2024-10-20 14:55:37 -0700

    error handling with case

--- lib/cards.ex
+++ lib/cards.ex
@@ -15,10 +15,18 @@ defmodule Cards do
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
-
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
+  end
+  def load(filename) do
+    # binary = File.read!(filename)
+    {status, binary} = File.read(filename)
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> raise "Error reading file: #{status}" # this looks like a formal error version
+    end
+    # :erlang.binary_to_term(binary)
   end
 
   # def deal_card(deck) do
