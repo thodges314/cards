e6c9222a0748a3f9fca37693af6e7ceebcd1cd30
Thomas Hodges <thodges@javascriptonline.org>
2024-10-19 22:18:26 -0700

    shuffle method

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,6 +2,13 @@ defmodule Cards do
   def create_deck do
     ["Ace", "Two", "Three"]
   end
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  # def deal_card(deck) do
+  #   deck |> Enum.pop
+  # end
 
 end
 
