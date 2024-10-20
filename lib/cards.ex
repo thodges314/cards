201f8cad470c6234f75ba1d5af40d4699db70df0
Thomas Hodges <thodges@javascriptonline.org>
2024-10-19 21:44:29 -0700

    start create deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,8 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def create_deck do
+    ["Ace", "Two", "Three"]
   end
+
 end
+
+# iex -S mix to start shell
