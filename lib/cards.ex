3bd254f50a0bb038a2f4e7fe652250c73b219aab
Thomas Hodges <thodges@javascriptonline.org>
2024-10-20 17:45:59 -0700

    doc tests and tests

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,6 +1,12 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards.
+  """
+  @doc """
+    Returns a list of strings representing a deck of playing cards.
+  """
   def create_deck do
-    values = ["Ace", "Two", "Three", "Four", "Five"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
@@ -9,9 +15,26 @@ defmodule Cards do
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+  @doc """
+    Determines wheather a deck contains a given card.
+
+  ## Examples
+      iex> deck = Cards.create_deck()
+      iex> Cards.contains?(deck, "Ace of Hearts")
+      true
+  """
   def contains?(deck, hand) do
     Enum.member?(deck, hand)
   end
+  @doc """
+    Divides a deck into a hand and the remainer of the deck.  The `hand_size` argument indicates how many cards should me in the hand.
+
+  ## Examples
+      iex> deck = Cards.create_deck()
+      iex> {hand, deck}=Cards.deal(deck,1)
+      iex> hand
+      ["Ace of Hearts"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
@@ -20,8 +43,6 @@ defmodule Cards do
     File.write(filename, binary)
   end
   def load(filename) do
-    # binary = File.read!(filename)
-    # {status, binary} =
     case File.read(filename) do
       {:ok, binary} -> :erlang.binary_to_term(binary)
       {:error, _reason} -> raise "Error reading file" # this looks like a formal error version
