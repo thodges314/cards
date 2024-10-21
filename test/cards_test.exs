8415030dbfa4d635b0f61e8e48c0d8bc0bc2e77c
Thomas Hodges <thodges@javascriptonline.org>
2024-10-20 18:04:09 -0700

    more test

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,5 +2,16 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
+  test "create_deck maked 52 cards" do
+    deck_length = length(Cards.create_deck())
+    assert deck_length == 52
+  end
+
+  test "shuffling a deck randomizes it" do
+    deck = Cards.create_deck()
+    shuffled_deck = Cards.shuffle(deck)
+    refute deck == shuffled_deck
+  end
+
 
 end
